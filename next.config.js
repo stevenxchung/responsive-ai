6634f6aabaaec25da9cc6083025ce92b26e4d338
Steven <stevenxchung@gmail.com>
2023-01-07 11:42:10 -0500

    Test rewrite
    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,20 @@
 /** @type {import('next').NextConfig} */
+const baseUrl = process.env.NEXT_PUBLIC_SERVER_URL
+
 const nextConfig = {
   reactStrictMode: true,
   // Reference: https://frontend-digest.com/environment-variables-in-next-js-9a272f0bf655
   env: {
-    SERVER_URL: process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:3000/',
+    SERVER_URL: baseUrl || 'http://localhost:3000/',
     OPENAI_API_KEY: process.env.OPENAI_API_KEY,
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/api/gpt-ai',
+        destination: `${baseUrl}/api/gpt-ai`,
+      },
+    ]
   },
 }
 
